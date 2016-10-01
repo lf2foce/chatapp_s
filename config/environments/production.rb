6abd3a0a45b3959a6e2756176ddb7a92a97c8d6f
Anh Bui <lf2foce@gmail.com>
2016-10-01 19:56:33 -0400

    awfawe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ config.action_cable.allowed_request_origins = ['https://snowchat.herokuapp.com',
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  # config.action_cable.url = 'wss://snowchat.herokuapp.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
