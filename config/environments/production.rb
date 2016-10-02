bcb2e2a74ac5419c7b9374b5f4521a72362705c4
Anh Bui <lf2foce@gmail.com>
2016-10-02 10:56:47 -0400

    chan asset comlipe to be true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ config.web_socket_server_url = "wss://snowchat.herokuapp.com/cable"
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
